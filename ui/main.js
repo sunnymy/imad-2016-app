ac04577a8e8a5bafcb0a2d650f1e180498e82b2e
Yash Modi <myn.yashnm@gmail.com>
2016-09-27 20:50:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var ani=document.getElementById('img');
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    ani.style.marginLeft = marginLeft + 'px';
+}
+
+ani.onclick = function(){
+    var interval = setINterval(moveRight,100);
+    
+};
