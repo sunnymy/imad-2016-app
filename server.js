61b73271d1fafba9341cfdc4ef20ee41b449a443
Yash Modi <myn.yashnm@gmail.com>
2016-09-27 20:01:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/sunny.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'sunny.jpg'));
+app.get('/ui/sun.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sun.jpg'));
 });
 
 app.get('/art-one',function(req,res){
