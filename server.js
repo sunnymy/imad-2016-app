f0ccc974450e5378fefa921dca5251d750681074
Yash Modi <myn.yashnm@gmail.com>
2016-09-27 20:11:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.get('/art-one',function(req,res){
     res.send('My profile');
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
